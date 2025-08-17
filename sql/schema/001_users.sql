e640dcf30cc5be2154601409aeadbaccfadbc4c2
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2025-08-17 09:04:47 +0800

    sql: creating table should use gen_random_uuid so we do not have to do that in code
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 CREATE TABLE users (
-	id	UUID	PRIMARY KEY,
+	id	UUID PRIMARY KEY DEFAULT gen_random_uuid (),
 	created_at	TIMESTAMP	NOT NULL,
 	updated_at	TIMESTAMP	NOT NULL,
 	email		TEXT	NOT NULL,
