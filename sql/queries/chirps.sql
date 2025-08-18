1cfc2720af1d56c231f2c22b34e6b50304998b0c
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2025-08-18 14:07:22 +0800

    feat: add more query for chirps
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- sql/queries/chirps.sql
+++ sql/queries/chirps.sql
@@ -19,3 +19,8 @@ WHERE id=$1 LIMIT 1;
 -- name: DeleteChirp :exec
 DELETE FROM chirps
 WHERE id=$1;
+
+-- name: GetChirpsByUserID :many
+SELECT * FROM chirps
+WHERE user_id=$1
+ORDER BY created_at ASC;
